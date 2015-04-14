0ca913a95123f8f44acb1d0699d39262d072bc3d
Gabriel Falkenberg <gabriel.falkenberg@gmail.com>
2015-04-14 15:10:50 +0200

    Basic tests for example app.

---
+++ test/exampleAppTest.js
@@ -0,0 +1,46 @@
+var request = require("supertest");
+var cheerio = require("cheerio");
+
+describe("Example App", function () {
+  var exampleApp, originalCwd;
+
+  before(function () {
+    originalCwd = process.cwd();
+
+    // The example application must be started from its own directory
+    process.chdir(__dirname + "/example-app");
+    exampleApp = require("./example-app/app.js");
+  });
+
+  after(function () {
+    process.chdir(originalCwd);
+  });
+
+  it("Serves busted client side resources", function (done) {
+    request(exampleApp)
+      .get("/")
+      .expect(/\/css\/main.css\?c=[0-9a-f]{32}/)
+      .expect(/\/js\/main.js\?c=[0-9a-f]{32}/)
+      .end(done);
+  });
+
+  it("Serves the client side resources with long cache headers", function (done) {
+    request(exampleApp)
+      .get("/")
+      .end(function (err, response) {
+        if (err) return done(err);
+
+        return request(exampleApp)
+          .get(getStylesheetPath(response))
+          .expect("Cache-Control", /max-age=31536000/)
+          .end(done);
+      });
+  });
+
+});
+
+function getStylesheetPath(response) {
+  var $ = cheerio.load(response.text);
+
+  return $("link[rel=stylesheet]").attr("href");
+}
