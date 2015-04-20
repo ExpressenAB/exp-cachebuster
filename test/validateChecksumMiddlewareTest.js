53f7f4933c2471bd5f9f9b0029762700362a767a
Fredrik Ronnerberg <fredrik@stockholmcode.se>
2015-04-20 10:14:55 +0200

    Made it impossible for the middleware to trigger synchronous fs.
    Also removed some unnecessary functionality. Checksums are only calculated when bust is called.

--- test/validateChecksumMiddlewareTest.js
+++ test/validateChecksumMiddlewareTest.js
@@ -1,31 +1,39 @@
 var fs = require("fs");
 var path = require("path");
+var sinon = require("sinon");
 var express = require("express");
 var request = require("supertest");
 
-// In a real application you would use require("exp-cacheBuster")({baseDirs: ...})
-var cacheBuster = require("../")({baseDirs: ["tmp"]});
-
-var checksum;
+var tmpFileChecksum;
+var checksums;
+var middleware;
 var tmpPath = path.join(__dirname, "../tmp");
 var tmpFileName = "/foo.css";
 var tmpFilePath = path.join(tmpPath, tmpFileName);
+
+var cacheBuster;
 
 describe("ValidateChecksumMiddleware", function () {
 
   before(function () {
     createTempFiles();
-    checksum = cacheBuster.checksum(tmpFileName);
+
+    cacheBuster = require("../")(["tmp"]);
+    tmpFileChecksum = cacheBuster.checksum(tmpFileName);
+    checksums = {};
+    checksums[tmpFilePath] = tmpFileChecksum;
+
+    middleware = cacheBuster.validateChecksumMiddleware(checksums);
   });
 
-  afterEach(function () {
-    cacheBuster.clearChecksumCache();
+  beforeEach(function () {
+    cacheBuster = require("../")(["tmp"]);
   });
 
   describe("Changes Cache-Control header", function () {
     it("sets the Cache-Control header to no-cache when checksums doesn't match", function (done) {
       var server = express();
-      server.use(cacheBuster.validateChecksumMiddleware());
+      server.use(middleware);
       server.get(tmpFileName, function (req, res) {
         res.set("Cache-Control", "public, max-age=100");
         res.sendFile(tmpFilePath);
@@ -37,9 +45,39 @@ describe("ValidateChecksumMiddleware", function () {
         .expect(200, done);
     });
 
-    it("sets the Cache-Control header to no-cache when query parameter c is missing", function (done) {
+    it("sets the Cache-Control header to no-cache when the requested checksum is not found", function (done) {
       var server = express();
-      server.use(cacheBuster.validateChecksumMiddleware());
+      server.use(middleware);
+      server.get("/missing-file?c=probably-valid-checksum", function (req, res) {
+        res.set("Cache-Control", "public, max-age=100");
+        res.sendStatus(404);
+      });
+
+      request(server)
+        .get("/missing-file" + "?c=some-checksum")
+        .expect("Cache-Control", "no-cache, no-store, must-revalidate")
+        .expect(404, done);
+    });
+  });
+
+  describe("Doesn't changes Cache-Control header", function () {
+    it("does nothing to Cache-Control header when checksum matches", function (done) {
+      var server = express();
+      server.use(middleware);
+      server.get(tmpFileName, function (req, res) {
+        res.set("Cache-Control", "public, max-age=100");
+        res.sendFile(tmpFilePath);
+      });
+
+      request(server)
+        .get(tmpFileName + "?c=" + tmpFileChecksum)
+        .expect("Cache-Control", "public, max-age=100")
+        .expect(200, done);
+    });
+
+    it("does nothing to the Cache-Control header when query parameter c is missing", function (done) {
+      var server = express();
+      server.use(middleware);
       server.get(tmpFileName, function (req, res) {
         res.set("Cache-Control", "public, max-age=100");
         res.sendFile(tmpFilePath);
@@ -47,53 +85,71 @@ describe("ValidateChecksumMiddleware", function () {
 
       request(server)
         .get(tmpFileName)
-        .expect("Cache-Control", "no-cache, no-store, must-revalidate")
-        .expect(200, done);
-    });
-  });
-
-  describe("Doesn't changes Cache-Control header", function () {
-    it("does nothing to Cache-Control header when checksum matches", function (done) {
-      var server = express();
-      server.use(cacheBuster.validateChecksumMiddleware());
-      server.get(tmpFileName, function (req, res) {
-        res.set("Cache-Control", "public, max-age=100");
-        res.sendFile(tmpFilePath);
-      });
-
-      request(server)
-        .get(tmpFileName + "?c=" + checksum)
         .expect("Cache-Control", "public, max-age=100")
         .expect(200, done);
-    });
-
-    it("does nothing when file cannot be found", function (done) {
-      var server = express();
-      server.use(cacheBuster.validateChecksumMiddleware());
-      server.get("/missing-file", function (req, res) {
-        res.set("Cache-Control", "public, max-age=100");
-        res.sendStatus(404);
-      });
-
-      request(server)
-        .get("/missing-file" + "?c=some-checksum")
-        .expect("Cache-Control", "public, max-age=100")
-        .expect(404, "Not Found", done);
     });
   });
 
   it("does not include the mounting point when resolving file path", function (done) {
     var server = express();
-    server.use("/static", cacheBuster.validateChecksumMiddleware());
+    server.use("static", middleware);
     server.get("/static" + tmpFileName, function (req, res) {
       res.set("Cache-Control", "public, max-age=100");
       res.sendFile(tmpFilePath);
     });
 
     request(server)
-      .get("/static" + tmpFileName + "?c=" + checksum)
+      .get("/static" + tmpFileName + "?c=" + tmpFileChecksum)
       .expect("Cache-Control", "public, max-age=100")
       .expect(200, done);
+  });
+
+  it("does not try to access the fs at any time", function (done) {
+    var readSpy = sinon.spy(fs, "readFileSync");
+    var existsSpy = sinon.spy(fs, "existsSync");
+
+    var server = express();
+    server.use("/", middleware);
+    server.get(tmpFileName, function (req, res) {
+      res.set("Cache-Control", "public, max-age=100");
+      res.sendFile(tmpFilePath);
+    });
+
+    request(server)
+      .get(tmpFileName + "?c=" + tmpFileChecksum)
+      .expect(200, function (err) {
+        if (err) {done(err);}
+      });
+    sinon.assert.notCalled(readSpy);
+    sinon.assert.notCalled(existsSpy);
+
+    request(server)
+      .get(tmpFileName + "?c=invalid-checksum")
+      .expect(200, function (err) {
+        if (err) {done(err);}
+      });
+    sinon.assert.notCalled(readSpy);
+    sinon.assert.notCalled(existsSpy);
+
+    request(server)
+      .get(tmpFileName)
+      .expect(200, function (err) {
+        if (err) {done(err);}
+      });
+    sinon.assert.notCalled(readSpy);
+    sinon.assert.notCalled(existsSpy);
+
+    request(server)
+      .get("/not-found")
+      .expect(404, function (err) {
+        if (err) {done(err);}
+      });
+    sinon.assert.notCalled(readSpy);
+    sinon.assert.notCalled(existsSpy);
+
+    fs.readFileSync.restore();
+    fs.existsSync.restore();
+    done();
   });
 });
 
